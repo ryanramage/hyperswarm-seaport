58b7a42050fc9022196744fec42864ee5cda3f88
Ryan Ramage <ryanr@redmantech.com>
2022-02-05 09:57:32 -0700

    test request

--- example/client.js
+++ example/client.js
@@ -1,9 +1,12 @@
 import http from 'http'
 import got from 'got'
 import Seaport from '../index.js'
+import request from 'request'
 
 const pubkey = process.argv[2]
 const ports = new Seaport(pubkey)
 
 const {host, port} = await ports.get('web@1.2.x')
-got.stream(`http://${host}:${port}`).pipe(process.stdout)
+request(`http://${host}:${port}`).pipe(process.stdout).on('close', () => {
+  console.log('done')
+})
